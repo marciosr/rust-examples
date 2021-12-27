47e2986e1a8c7c80c3fb8b46774e710a8e42514b
Márcio <marcio@archdesk.localdomain>
2021-12-27 18:20:41 -0300

    Atualiza a descrição de cada exemplo no módulo de vetores

--- src/vec/bin/main.rs
+++ src/vec/bin/main.rs
@@ -8,6 +8,7 @@ fn main() {
 	vec_retain();
 	vec_leak ();
 	vec_split_off ();
+	vec_pop();
 }
 
 fn vec_parts () {
@@ -42,22 +43,60 @@ fn vec_parts () {
 fn vec_retain () {
 	let mut vec = vec![1, 2, 3, 4, 5, 6, 7];
 	vec.retain(|&x| x % 2 == 0);
-	println!("\nVetor orgininal: [1, 2, 3, 4, 5, 6, 7]\nResultado do método vec.retain(): {:?}", vec);
+	println!("\n\n  Exemplo de uso do método vec.retain()
+	Vetor orgininal: [1, 2, 3, 4, 5, 6, 7]
+	vec.retain(|&x| x % 2)
+	Método vec.retain() filtrando apenas os números divisíveis por 2.
+	Resultado	: {:?}", vec);
 }
 
-
+// Consome um vetor e retorna uma referência mutável para
+// seu conteúdo.
+// No exemplo abaixo um vetor imutável pôde ser alterado
+// apartir do retorno do método .leak().
+// O vetor original foi consumido, portanto não pode ser mais utilizado.
 fn vec_leak () {
 	let x = vec![1, 2, 3];
 	let static_ref: &'static mut [usize] = x.leak();
 	
-	static_ref[0] += 1;
-	
-	println!("\nResultado do método vec.leak(): {:?}", static_ref);
+	static_ref[0] += 20;
+
+	println!("\n\n  Exemplo de uso do método vec.leak()
+	Vetor original [1, 2, 3]
+	Aplicando static_ref[0] += 1;
+	Resultado do método vec.leak(): {:?}", static_ref);
 }
 
+// Divide um vetor removendo seu conteúdo a partir de uma posição informada e
+// atribuindo o restante a um novo vetor.
 fn vec_split_off () {
-	let mut vec = vec![1, 2, 3, 4, 5];
+	let mut vec = vec![12, 45, 88, 19, 13];
 	let vec2 = vec.split_off(2);
 	
-	println!("\nResultado do vec.split_off()\nVetor original: {:?}\nVetor receptor: {:?}", vec, vec2);
+	println!("\n\n  Exemplo de uso do método vec.split_off()
+	Vetro original: [1, 2, 3, 4, 5]
+	Resultado do vec.split_off() a partir da posição 2
+	Vetor original: {:?}
+	Vetorreceptor: {:?}", vec, vec2);
+}
+
+
+// Método .pop()
+// Retorna o último elemento de um vetor e o retorna, ou retorna None se estiver vazio
+// Retorna Some(T) ou None.
+fn vec_pop () {
+	let mut vec = vec![12, 45, 88];
+
+	let a = vec.pop();
+	let b = vec.pop();
+	let c = vec.pop();
+	let d = vec.pop();
+
+	println!("\n\n  Método pop().
+	Vetor original = [12, 45, 88]
+	Resultado da primeira aplicação: {:?}
+	Resultado da segunda aplicação: {:?}
+	Resultado da terceira aplicação: {:?}
+	Resultado da quarta aplicação: {:?}", a.unwrap(), b, c, d);
+
 }
